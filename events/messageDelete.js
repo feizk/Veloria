97fed678bf6897f76e54a7afa00cfb869e8e3e97
Feizk <rashydhrayan3@gmail.com>
2025-12-17 09:10:32 +0000

    fix: Enhance message delete handling to include counting logic and guild data retrieval

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,5 +1,6 @@
 const { Events } = require("discord.js");
 const { sendLog } = require("../helpers/message");
+const Guild = require("../models/Guild");
 
 module.exports = {
   name: Events.MessageDelete,
@@ -13,5 +14,18 @@ module.exports = {
       extra: `${message.cleanContent}`,
       footer: `mid: ${message.id} aid: ${message.author.id}`,
     });
+
+    const guildData = await Guild.findOne({ id: message.guildId });
+    if (!guildData) return;
+
+    if (guildData.counting.enabled) {
+      if (message.channelId != guildData.counting.channel) return;
+
+      if (Number(message.content) === guildData.counting.count) {
+        return message.channel.send(
+          `${message.author} | Please don't delete your message.\n> Continue from ${guildData.counting.count}`,
+        );
+      }
+    }
   },
 };
