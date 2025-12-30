15d2df2672f97d0e6212aac81858df0df03f3e62
Feizk <rashydhrayan3@gmail.com>
2025-12-30 22:07:20 +0000

    feat: Implement info command with user data handling and welcome message

---
+++ commands/messages/index.js
@@ -0,0 +1,23 @@
+const { getArgs, validateID } = require("../../helpers/message");
+const User = require("../../models/User");
+
+/**
+ * Arguments; user(1)
+ * @param {import("discord.js").Message} message 
+ */
+module.exports = async (message) => {
+    const args = getArgs(message.content);
+    let userId = args.result.user?.at(0)?.value;
+    if (!userId) userId = message.author.id;
+
+    if (!validateID(userId))
+        message.reply(`:x: | Invalid discord user ID`);
+
+
+    let userData = await User.findOne({ id: userId, guild: message.guildId });
+    if (!userData) {
+        userData = await User.create({ id: userId, guild: message.guildId });
+    }
+
+    return message.reply(`💬 | <@${userId}> has sent **${userData.messages}** messages in this server!`)
+}
